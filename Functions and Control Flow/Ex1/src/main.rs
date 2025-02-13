3b56bc54da28ebe7eb7b4b176f495576cc884ff8
Clément <clementconrie@gmail.com>
2025-02-13 17:44:48 +0100

    Ajout Exercice 1 & 4

---
+++ Functions and Control Flow/Ex1/src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn saisie_longueur() -> f32 {
+    let mut longueur = String::new();
+    loop {
+        println!("Saisissez la longueur du rectangle :");
+        io::stdin().read_line(&mut longueur).expect("Erreur lors de la lecture...");
+        match longueur.trim().parse::<f32>() {
+            Ok(n) => return n,
+            Err(_) => {
+                println!("Saisissez un nombre !");
+                continue;
+            }
+        };
+    }
+}
+
+fn saisie_largeur() -> f32 {
+    let mut largeur = String::new();
+    loop {
+        println!("Saisissez la largeur du rectangle :");
+        io::stdin().read_line(&mut largeur).expect("Erreur lors de la lecture");
+        match largeur.trim().parse::<f32>() {
+            Ok(n) => return n,
+            Err(_) => {
+                println!("Saisissez un nombre");
+                continue;
+            }
+        }
+    }
+}
+
+fn calcul_aire(longueur:f32, largeur:f32) -> f32 {
+    longueur * largeur
+}
+
+fn affichage(longueur:f32, largeur:f32, aire:f32) {
+    println!("La longueur est {}, la largeur {}, et l'aire {}.", longueur, largeur, aire)
+}
+
+fn main() {
+    let longueur:f32 = saisie_longueur();
+    let largeur:f32 = saisie_largeur();
+    let aire:f32 = calcul_aire(longueur, largeur);
+    affichage(longueur, largeur, aire)
+}
---
+++ Functions and Control Flow/Ex4/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn saisie_nombre() -> u32 {
+    let mut n = String::new();
+    loop {
+        println!("Saisissez un entier positif:");
+        io::stdin().read_line(&mut n).expect("Erreur de lecture...");
+        match n.trim().parse() {
+            Ok(n) => return n,
+            Err(_) => {
+                println!("Veuillez saisir un nombre entier positif !");
+                continue
+            }
+        }
+    }
+}
+
+fn calcul_factorielle(n:u32) -> u32{
+    let mut factorielle:u32 = 1;
+    for i in 1..=n {
+        factorielle *=i;
+    }
+    factorielle
+}
+
+fn affichage(n:u32, factorielle:u32) {
+    println!("La factorielle de {} est : {}", n, factorielle)
+}
+fn main() {
+    let n:u32 = saisie_nombre();
+    let factorielle:u32 = calcul_factorielle(n);
+    affichage(n, factorielle)
+}
